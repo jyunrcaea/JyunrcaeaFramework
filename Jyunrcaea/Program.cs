ec0dd2e5e45d86f47c8f9463e1ac6394cc3032a0
Jyunni <71429443+614project@users.noreply.github.com>
2022-12-25 17:15:10 +0900

    누락된 파일 추가

---
+++ Jyunrcaea/Program.cs
@@ -0,0 +1,56 @@
+﻿using JyunrcaeaFramework;
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+        Framework.Init("Jyunrcaea! Framework Test", 720, 480, null, null, new(true, false, false, false, true));
+        Framework.function = new CustomFrameworkFunction();
+        Display.AddScene(new MainScene());
+        Framework.Run();
+    }
+}
+
+class CustomFrameworkFunction : FrameworkFunction
+{
+    public override void Start()
+    {
+        base.Start();
+        Window.Show = true;
+    }
+
+    public override void WindowQuit()
+    {
+        base.WindowQuit();
+        Window.Show = false;
+        Framework.Stop();
+    }
+}
+
+class MainScene : Scene
+{
+    public MainScene()
+    {
+        this.AddSprite(new TimeText());
+    }
+}
+
+class TimeText : TextBox, UpdateEventInterface
+{
+    public TimeText() : base("font.ttf",(int)(Window.AppropriateSize * 25))
+    {
+
+    }
+
+    public override void Resize()
+    {
+        this.Size = (int)(Window.AppropriateSize * 25);
+        base.Resize();
+    }
+
+    public void Update(float ms)
+    {
+        this.Text = $"{DateTime.Now.ToLongDateString()} {DateTime.Now.ToLongTimeString()}";
+    }
+}
