62190c048e36fec8bd75fb033576c65d8785429e
614project <71429443+614project@users.noreply.github.com>
2026-03-13 01:33:20 +0900

    exclude

--- TestApp/Program.cs
+++
@@ -1,9 +0,0 @@
-using JyunrcaeaFrameworkApp.Editor;
-
-RenderOption renderOption = new(false, true);
-
-Framework.Init("Jyunrcaea Framework App", 1280, 720, renderOption: renderOption);
-Font.DefaultPath = @"Fonts\SUIT-Variable.ttf";
-Display.Target.Objects.Add(new EditorScene());
-Display.FrameLateLimit = (Display.FrameLateLimit = 0) * 2;
-Framework.Run();
